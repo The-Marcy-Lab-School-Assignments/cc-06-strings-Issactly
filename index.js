b3d8e5fba5ca30a25b442291911bd1596c2aaa58
Issac <IssacBienA@gmail.com>
2024-10-10 13:36:26 -0400

    Update index.js
    Completed problem ONE working on problem TWO

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //write your code here
+//1
+const reverseString = (str) => {
+  let reverse = ""
+  for (let i = str.length -1; i>= 0; i--) {
+    reverse += str[i];
+  }
+  return r;
+};
+//console.log(reverseString('Marcy Lab"))
+
+//2
+const reverseZigZagString = (str) => {
+  let reverse = ""
+  for (let i = str.length -1; I>=0; i--) {
+    reverse += str[i];
+  }
+  for (let i % 2 === 0 
